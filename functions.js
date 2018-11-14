b4fcbe2462ad654bb4e4c010fc0a156579401cb9
Lee Zhi Jiang <leezhijiang@1utar.my>
2018-11-14 20:31:04 +0800

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,29 @@
+function openMenu(menuname) {
+  var i;
+  var x = document.getElementsByClassName("menu");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(menuname).style.display = "block";
+  if(menuname=="show"){
+    showall();
+  }
+}
+
+function generate(){
+var size=list.length;
+var index= Math.floor(Math.random() * size);
+document.getElementById("truth_dare_content").innerHTML = list[index];
+}
+
+function showall(){
+  var ul = document.querySelector("ul");
+
+for (var i = 0; i < list.length; i++) {
+    var item = list[i];
+    var listItem = document.createElement("li");
+    listItem.setAttribute("class", "list-item");
+    listItem.textContent = item;
+    ul.appendChild(listItem);
+  }
+}
